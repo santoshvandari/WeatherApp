ebf0b40f0968ec78247ba200035bb20d9fb288c7
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-27 09:49:11 +0545

    Initial

--- WeatherApp/urls.py
+++ WeatherApp/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,redirect
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     # path("",redirect("Index.urls")),
 ]
