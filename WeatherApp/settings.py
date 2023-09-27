ace898bb24c80ebf127baa653595031dd171a64f
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-27 09:36:54 +0545

    Initial

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'WeatherApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'WeatherApp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
