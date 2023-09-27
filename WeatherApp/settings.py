bf97cdd2360c8e093e8dc6c3fe1dd98b2d50b16d
Santosh Bhandari <santoshvandari1@gmail.com>
2023-09-27 09:38:58 +0545

    Initial

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'index'
+    'Index'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
